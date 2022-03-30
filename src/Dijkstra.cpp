a458ab7bad9977acad42f48b7dfdabdc98039176
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-03-30 09:48:33 +0200

    roxygen documentation

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -4,7 +4,10 @@
 
 using namespace std;
 
-
+//' Dijkstra algorithm implementation
+//' @param mat the Graph structure representation as output from as_edgelist(G)
+//' @param weights a vector containing the weights for each edge(each line) of
+//'       the edgelist matrix
 // [[Rcpp::export]]
 int dijkstra(Rcpp::NumericMatrix mat, std::vector<int> weights){
 
