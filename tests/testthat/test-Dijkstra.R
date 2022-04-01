bc2c6678bf6586f682d316d4e95d08cfa3314f19
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-04-01 13:34:21 +0200

    ring graph test

--- tests/testthat/test-Dijkstra.R
+++ tests/testthat/test-Dijkstra.R
@@ -21,16 +21,40 @@ test_that(
 )
 
 test_that(
-    "Sample graph 2",
+    "Sample graph 2 (Zachary)",
     {
         result = TRUE
 
         G <-  graph("Zachary")      # sample graph
-        distanceVector <- dijkstraSparseMatrix(as_adj(G), "1")
 
+        distanceVector <- dijkstraSparseMatrix(as_adj(G), "1")
         correctVector <- setNames(
             c(0,1,1,1,1,1,1,1,1,2,1,1,1,1,3,3,2,1,3,1,3,1,3,3,2,2,3,2,2,3,2,1,2,2),
             1:34
+        )
+
+
+        for(name in names(distanceVector)) {
+            result = result && (distanceVector[[name]] == correctVector[[name]])
+        }
+
+        expect_true(result)
+
+    }
+)
+
+test_that(
+    "Sample graph 3 (ring)",
+    {
+        result = TRUE
+
+        G <- make_ring(20)
+        E(G)$weight <- c(2,1,1,3,5,2,1,1,4,3,5,2,1,1,3,2,1,1,3,8)
+
+        distanceVector <- dijkstraSparseMatrix(as_adj(G, attr = "weight"), "1")
+        correctVector <- setNames(
+            c(0,2,3,4,7,12,14,15,16,20,23,22,20,19,18,15,13,12,11,8),
+            1:20
         )
 
 
